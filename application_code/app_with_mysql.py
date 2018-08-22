f1a5dd97038c92688d2f3ee48ea1c9bddab51fd3
root <root@197blrnb21853>
2018-08-22 12:13:26 +0530

    fixed the multithread race condition

--- application_code/app_with_mysql.py
+++ application_code/app_with_mysql.py
@@ -5,10 +5,12 @@ from flask import abort
 from flask import request
 from flask import Flask, render_template
 import pymysql.cursors 
+import shutil
 import string
 import random
 import json
 pymysql.install_as_MySQLdb()
+from pathlib import Path
 
 app = Flask(__name__)
 
@@ -16,7 +18,7 @@ def dbConnection():
     #db = pymysql.connect("127.0.0.1","root","Bablu@143341","templates" )
     db_host="127.0.0.1"
     db_user="root"
-    db_password="MYSQL_PASSWORD"
+    db_password="Mysql_password"
     db_name="templates"
     db = pymysql.connect(db_host,db_user,db_password,db_name )
     #db = pymysql.connect(db='templates', user='root', passwd='root', host='127.0.0.1', port=3306)
@@ -62,38 +64,29 @@ def get_task(template_id):
     except:
         return jsonify({"Msg":"Template Not Found."})
 
-# --- to get random template name----
-def get_last_template_id():
-    conn = dbConnection()
-    cur = conn.cursor()
-    try:
-        cur.execute("select Template_Id from templates ORDER BY Template_Id DESC LIMIT 1")
-        rows = cur.fetchone()[0]
-        return rows
-    except:
-        return 0
-    
-
 @app.route('/templates', methods=['POST'])
 def create_task():
-    #if not request.json or not 'title' in request.json:
-    #    abort(400)
-    last_template_id = get_last_template_id() + 1
-    name = "Template_"+str(last_template_id)
-    render = 0
     conn = dbConnection()
     cur = conn.cursor()
-    query = "INSERT INTO templates (Template_Name,render_status) VALUES ('"+name+"',"+str(render)+")"
-    cur.execute(query)
-    conn.commit()
-    readFile = open("templates/base.html", "r")
-    templateData = readFile.read()
-    templateName = "templates/"+name + ".html"
-    file = open(templateName,"w")
-    file.write(templateData)
-    file.close()
-    return jsonify({'msg': "A new template is created","Template Id": last_template_id,"name": name}), 201
-
+    try :
+        cur.execute("select Template_Id from templates ORDER BY Template_Id DESC LIMIT 1")
+        last_template_id = cur.fetchone()[0] + 1
+    except: 
+        last_template_id = 1
+    name = "Template_"+str(last_template_id)
+    render = 0
+    try:
+        baseTemplate = Path("templates/base.html")
+        if conn and baseTemplate.is_file():
+            query = "INSERT INTO templates (Template_Name,render_status) VALUES ('"+name+"',"+str(render)+")"
+            cur.execute(query)
+            conn.commit()
+            shutil.copy('templates/base.html', 'templates/'+name+".html")
+            return jsonify({'msg': "A new template is created","Template Id": last_template_id,"name": name}), 201
+        else:
+            return jsonify({"Error":"Unable to create a Template"})
+    except:
+        return jsonify({"Error":"Unable to create a Template"})
 if __name__ == '__main__':
     app.run(debug=True)
 
