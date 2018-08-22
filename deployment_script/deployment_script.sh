3ec5a3173f20a2671b8e52e12b44fdba48e02b62
root <root@197blrnb21853>
2018-08-22 12:16:14 +0530

    fixed the multithread race condition

--- deployment_script/deployment_script.sh
+++ deployment_script/deployment_script.sh
@@ -3,5 +3,12 @@
 virtualenv flash
  git clone https://github.com/nagaraju0wifi/messagepoint_test.git
  cd messagepoint_test
- python app.py
+ pip install pathlib
+ pip install pymysql
+ pip install json
+ pip install shutil
+ pip install string
+ pip install random
+
+ python app_with_*.py
 
